cfa125ac4ae0397ce5f3d233128622c5ecf6ba2d
Nikk45 <nicksin36@gmail.com>
2023-09-01 18:19:49 +0530

    submit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
 
-import React from "react";
+import React, { useContext } from "react";
 import './../styles/App.css';
+import DataContext from "./DataContext";
 
 const App = () => {
+
+
+  const {data, setData} = useContext(DataContext);
+
+  if(data.length === 0){
+
+    fetch("https://dummyjson.com/products")
+    .then((res)=>res.text())
+    .then((res)=>setData(res))
+  }
+
+  
+  console.log(typeof data);
+
   return (
-    <div>
+
+      <div>
         {/* Do not remove the main div */}
-    </div>
+        {
+          data.length===0 && <p>Loading...</p>
+        }
+        {
+          data && <div>
+            <h1>Data Fetched from API</h1>
+            <pre>{data}</pre>
+          </div>
+        }
+
+      </div>
+  
   )
 }
 
---
+++ src/components/DataProvider.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import DataContext from "./DataContext";
+
+const DataProvider=(props)=>{
+
+    const [data, setData] = useState('')
+
+    
+
+
+    return (
+        <DataContext.Provider value={
+                {
+                    data: data,
+                    setData: setData
+                }
+            }>
+            {props.children}
+        </DataContext.Provider>
+    )
+}
+
+
+export default DataProvider
